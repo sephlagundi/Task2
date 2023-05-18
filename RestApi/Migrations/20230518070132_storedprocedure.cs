77a5d1900b3ce897c6ba97d94c19bf8ffc0f9dab
joseph lagundi <sephmarchadesch@gmail.com>
2023-05-18 15:25:53 +0800

    Stored Procedure

---
+++ RestApi/Migrations/20230518070132_storedprocedure.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestApi.Migrations
+{
+    public partial class storedprocedure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"CREATE OR ALTER PROCEDURE getallemployees
+
+            AS
+
+            BEGIN
+                SELECT * FROM Employees;
+            END";
+
+            migrationBuilder.Sql(sp);
+          
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"DROP PROCEDURE getallemployees";
+
+
+            migrationBuilder.Sql(sp);
+
+        }
+    }
+}
